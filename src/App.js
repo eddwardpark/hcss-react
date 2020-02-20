501559dc34f92590eb5fde1fb6c9554b7a9fca40
eddwardpark <ngs9001@gmail.com>
2020-02-20 18:59:03 +0900

    Revert "reset"
    This reverts commit e3fbfd9df039b61c11dcf23e862039dc987b763f.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import HcssComponent from './component/HcssComponent'
+import LayoutComponent from './components/LayoutComponent'
 
 function App() {
 
     return (
         <div style={{ width: '100%', height: '100vh', backgroundColor: "lightgray" }}>
-            <HcssComponent
+            <LayoutComponent
                 size='pw50 wx500 wn300 ph60 hx400 hn300'
                 color='bc-green300 fc-yellow500 bdc-lightblue700'
                 space='ml4 mr8 mt16 mb24 pl3 pr6 pt9 pb12'
@@ -16,43 +16,43 @@ function App() {
                 position='pos-relative'
                 display='overflow-scroll'
             >
-                <HcssComponent
+                <LayoutComponent
                     layout='x-scroll'
                     style={{ width: 200, height: 40, backgroundColor: '#d1d1d1', margin: 4 }}
                 >
                     Layout1
-                </HcssComponent>
-                <HcssComponent
+                </LayoutComponent>
+                <LayoutComponent
                     layout='x-scroll'
                     style={{ width: 200, height: 40, backgroundColor: '#d1d1d1', margin: 4 }}
                 >
                     Layout2
-                </HcssComponent>
-                <HcssComponent
+                </LayoutComponent>
+                <LayoutComponent
                     layout='x-scroll'
                     style={{ width: 200, height: 40, backgroundColor: '#d1d1d1', margin: 4 }}
                 >
                     Layout3
-                </HcssComponent>
-                <HcssComponent
+                </LayoutComponent>
+                <LayoutComponent
                     layout='x-scroll'
                     style={{ width: 200, height: 40, backgroundColor: '#d1d1d1', margin: 4 }}
                 >
                     Layout4
-                </HcssComponent>
-                <HcssComponent
+                </LayoutComponent>
+                <LayoutComponent
                     layout='x-scroll'
                     style={{ width: 200, height: 40, backgroundColor: '#d1d1d1', margin: 4 }}
                 >
                     Layout5
-                </HcssComponent>
-                <HcssComponent
+                </LayoutComponent>
+                <LayoutComponent
                     layout='x-scroll'
                     style={{ width: 200, height: 40, backgroundColor: '#d1d1d1', margin: 4 }}
                 >
                     Layout6
-                </HcssComponent>
-            </HcssComponent>
+                </LayoutComponent>
+            </LayoutComponent>
         </div>
     );
 }
